aca9feb6409690369845e10eb85ac1bb97d20a0c
Cameron Henige <godmodecameron@gmail.com>
2021-06-26 00:38:54 -0400

    Initial Commit

---
+++ TestListApp/TestListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestListAppApp.swift
+//  TestListApp
+//
+//  Created by Hannah Krolewski on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct TestListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
